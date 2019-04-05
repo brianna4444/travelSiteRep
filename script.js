68cf14042ea752ecae1e733741deadd792e993cb
Brianna <althage.brianna@gmail.com>
2019-04-05 00:55:46 -0500

    finished gallery

---
+++ script.js
@@ -0,0 +1,34 @@
+let nameColl= "mexicoBtn";
+let cityName= "Riviera Maya";
+let cityImage= "images/mexicoBtn/RivieraMaya";
+let images = [];
+
+let mongo = require("mongodb").MongoClient;
+let mongodb = require("mongodb");
+let fs = require('fs');
+
+mongo.connect("mongodb://localhost:27017", function (err, client) {
+    if (err) {
+        console.log("cant connect to server");
+        return;
+    }
+
+    let db = client.db("config");
+    let coll = db.collection(nameColl);
+    fs.readdirSync(cityImage).forEach(file => {
+        console.log(file);
+        if (file !== ".DS_Store"){
+            images.push(cityImage + "/" + file);
+        }
+    });
+
+    let obj= {
+        "name": cityName,
+        "cityImage": images[0],
+        "images": images
+    }
+
+    coll.insertOne(obj);
+
+client.close();
+})
